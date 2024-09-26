614bf32e4227c5355094c2c769efb6c2a2ab9e8b
axect <axect.tg@proton.me>
2024-09-26 13:41:30 +0900

    Understand vmap with inner product

---
+++ 07_inner.py
@@ -0,0 +1,42 @@
+import jax
+import jax.numpy as jnp
+
+
+def dot(x,y):
+    return jnp.dot(x, y)
+
+x = jnp.asarray([
+    [2, 2, 2],
+    [3, 3, 3]
+])
+
+y = jnp.asarray([
+    [4, 4, 4],
+    [5, 5, 5]
+])
+
+#print(dot(x, y)) # : Error
+
+vdot = jax.vmap(dot, in_axes=[0, 0])
+print(vdot(x, y))
+# vdot(x, y) = [dot(x[0, :], y[0, :]), dot(x[1, :], y[1, :])]
+
+x_mat = jnp.arange(6).reshape(3, 2)
+y_vec = jnp.arange(2)
+y_mat = jnp.arange(6).reshape(2, 3)
+
+vv = lambda x, y: jnp.dot(x, y)
+mv = jax.vmap(vv, (0, None), 0)
+# mv(x, y) = [vv(x[0, :], y), vv(x[1, :], y), ...]
+mm = jax.vmap(mv, (None, 1), 1)
+# mm(x, y) = [mv(x, y[:, 0]), mv(x, y[:, 1]), ...]
+#          = [
+#             vv(x[0, :], y[:, 0]), vv(x[0, :], y[:, 1]), ...;
+#             vv(x[1, :], y[:, 0]), vv(x[1, :], y[:, 1]), ...
+#            ]
+
+print(f"x_mat:\n {x_mat}")
+print(f"y_vec: {y_vec}")
+print(f"x_mat * y_vec: {mv(x_mat, y_vec)}")
+print(f"y_mat:\n {y_mat}")
+print(f"x_mat * y_mat:\n {mm(x_mat, y_mat)}")
